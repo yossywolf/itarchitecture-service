ffacc0c33c46cd52e5a6c4ab6661b6797af3cf55
yossywolf <yossywolf@users.noreply.github.com>
2016-11-02 21:48:38 +0900

    Add REAL service status in activity!

--- app/src/main/java/com/example/g2115030/itarchitectureservice/MainActivity.java
+++ app/src/main/java/com/example/g2115030/itarchitectureservice/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.g2115030.itarchitectureservice;
 
+import android.app.ActivityManager;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.graphics.Color;
 import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,17 +13,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     private Button manageButton = null;
     private TextView statusText = null;
     private Intent intent = null;
-    private boolean mode = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        // UI部品を初期化
         manageButton = (Button)findViewById(R.id.serviceManageButton);
         manageButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,16 +34,16 @@ public class MainActivity extends AppCompatActivity {
                 toggleService();
             }
         });
-
         statusText = (TextView)findViewById(R.id.statusText);
+        setServiceStatusToActivity(checkServiceStatus());
 
 
         // サービスクラスを指定
         intent = new Intent(MyService.class.getName());
-
         intent.setPackage("com.example.g2115030.itarchitectureservice");
     }
 
+    // サービスを接続/接続解除した時の動作
     private ServiceConnection connect = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
@@ -51,20 +56,43 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-    private void toggleService() {
-        if (!mode) {
-            // サービスの起動
-            bindService(intent, connect, BIND_AUTO_CREATE);
+    // サービスの起動状態をチェックする https://goo.gl/UvGZb2
+    private boolean checkServiceStatus() {
+        ActivityManager am = (ActivityManager)this.getSystemService(ACTIVITY_SERVICE);
+        List<ActivityManager.RunningServiceInfo> listServiceInfo = am.getRunningServices(Integer.MAX_VALUE);
+        boolean found = false;
+
+        for(ActivityManager.RunningServiceInfo current : listServiceInfo) {
+            if(current.service.getClassName().equals(MyService.class.getName())) {
+                found = true;
+                break;
+            }
+        }
+        return found;
+    }
+
+    // UI部品を更新する
+    private void setServiceStatusToActivity(boolean status) {
+        if(status == true) {
             statusText.setText("Status: RUNNING");
+            statusText.setTextColor(Color.GREEN);
             manageButton.setText("STOP SERVICE");
-            mode = true;
         } else {
-            // サービスの停止
-            unbindService(connect);
             statusText.setText("Status: STOPPED");
+            statusText.setTextColor(Color.RED);
             manageButton.setText("RUN SERVICE");
-            mode = false;
         }
     }
 
+    // サービスの起動/終了を切り替える
+    private void toggleService() {
+        if (!checkServiceStatus()) {
+            // サービスの起動
+            bindService(intent, connect, BIND_AUTO_CREATE);
+        } else {
+            // サービスの停止
+            unbindService(connect);
+        }
+        setServiceStatusToActivity(checkServiceStatus());
+    }
 }
